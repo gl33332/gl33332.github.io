604be0a2919e22e3caa5413eb10766c65d7e191b
gl33332 <gl33332@gmail.com>
2025-06-02 01:20:05 +0800

    Site updated: 2025-06-02 01:20:05

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/30/私信介绍/","2025/06/01/Hexo-基础命令/","2025/06/02/影视tv/"];function toRandomPost(){
+var posts=["2025/05/30/私信介绍/","2025/06/01/Hexo-基础命令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
