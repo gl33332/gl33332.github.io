76dc1563f0d77ace33fcdb5cbc027684761b8818
gl33332 <gl33332@gmail.com>
2025-06-02 01:00:43 +0800

    Site updated: 2025-06-02 01:00:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["source/_posts/","source/_posts/"];function toRandomPost(){
+var posts=["source/_posts/","source/_posts/","source/_posts/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
